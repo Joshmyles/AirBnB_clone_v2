029adc0a4809f4994402e85bd1b3d4170391fc06
Joshmyles <joshmyles844@gmail.com>
2024-06-03 13:18:15 +0300

    Review class

--- models/review.py
+++ models/review.py
@@ -4,7 +4,14 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    """ Review class that stores review information """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
+
+    place_id = str  # id of the place being reviewed
+    user_id = str  # id of the user reviewing the place
+    text = str  # information of the review
